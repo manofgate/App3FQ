119ea3ef10acf4605724927693e4100244bd95da
Ben Casey <bcasey@mines.edu>
2013-11-06 15:29:35 -0700

    added manage mode button, main activity player

--- FreeganQuestCaseySoto/src/edu/mines/freeganquestcaseysoto/MainActivity.java
+++ FreeganQuestCaseySoto/src/edu/mines/freeganquestcaseysoto/MainActivity.java
@@ -1,21 +1,80 @@
 package edu.mines.freeganquestcaseysoto;
 
-import android.os.Bundle;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private Spinner mHunts;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mHunts = (Spinner)findViewById( R.id.selectHunts );
+		
+		ArrayList<String> arrayList1 = new ArrayList<String>();
+
+        arrayList1.add("Select a Hunt...");
+        arrayList1.add("Delhi");
+        ArrayAdapter<String> adp = new ArrayAdapter<String> (this,android.R.layout.simple_spinner_dropdown_item,arrayList1);
+        mHunts.setAdapter(adp);
+
+        mHunts.setVisibility(View.VISIBLE);
+       //Set listener Called when the item is selected in spinner
+       mHunts.setOnItemSelectedListener(new OnItemSelectedListener() 
+       {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                    int position, long arg3) 
+            {
+                String city = "The city is " + parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), city, Toast.LENGTH_LONG).show();
+
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) 
+            {
+                // TODO Auto-generated method stub
+            }
+        });
+		
 	}
 
 	@Override
+	  public boolean onOptionsItemSelected( MenuItem item )
+	  {
+	    switch( item.getItemId() )
+	    {
+	      case R.id.action_manage:
+	      {
+	        Intent i = new Intent(this, ManagerMain.class);
+	        startActivity(i);
+
+	        return true;
+	      }
+	      default:
+	          return super.onOptionsItemSelected(item);
+	    }
+	  }
+	
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mm, menu);
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
 	}
 
